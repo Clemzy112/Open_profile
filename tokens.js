e742439f7f217e80cfe915ffa6c992391cb09430
Open_MyTrust_profile <clementbtc2023@gmail.com>
2026-05-22 10:08:24 +0100

    Update tokens.js

--- tokens.js
+++ tokens.js
@@ -1,25 +1,48 @@
 const TOKENS = [
+
 {
 name: "TRON",
 symbol: "TRX",
 type: "native",
-logo: "https://cryptologos.cc/logos/tron-trx-logo.png",
-chart: "https://dexscreener.com/tron"
+
+logo:
+"https://raw.githubusercontent.com/trustwallet/assets/master/blockchains/tron/info/logo.png",
+
+chart:
+"https://dexscreener.com/tron",
+
 },
+
 {
 name: "Tether USD",
 symbol: "USDT",
 type: "trc20",
-address: "TR7NHqjeKQxGTCi8q8ZY4pL8otSzgjLj6t",
-logo: "https://cryptologos.cc/logos/tether-usdt-logo.png",
-chart: "https://dexscreener.com/tron"
+
+address:
+"TR7NHqjeKQxGTCi8q8ZY4pL8otSzgjLj6t",
+
+logo:
+"https://raw.githubusercontent.com/trustwallet/assets/master/blockchains/tron/assets/TR7NHqjeKQxGTCi8q8ZY4pL8otSzgjLj6t/logo.png",
+
+chart:
+"https://dexscreener.com/tron",
+
 },
+
 {
 name: "Wrapped TRON",
 symbol: "WTRX",
 type: "trc20",
-address: "WRAPPED_TRON_CONTRACT_ADDRESS_HERE",
-logo: "https://cryptologos.cc/logos/tron-trx-logo.png",
-chart: "https://dexscreener.com/tron"
+
+address:
+"TNUC9Qb1rRpS5CbWLmNMxXBjyFoydXjWFR",
+
+logo:
+"https://raw.githubusercontent.com/trustwallet/assets/master/blockchains/tron/info/logo.png",
+
+chart:
+"https://dexscreener.com/tron",
+
 }
+
 ];
