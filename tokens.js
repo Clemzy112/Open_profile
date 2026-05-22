4b95e01262f384d5acbe56fd85207ca145c9250a
Open_MyTrust_profile <clementbtc2023@gmail.com>
2026-05-22 04:53:41 +0100

    Create tokens.js

---
+++ tokens.js
@@ -0,0 +1,52 @@
+const TOKENS = [
+
+/* TRON */
+{
+name: "TRON",
+symbol: "TRX",
+type: "native",
+
+logo: "assets/tokens/trx.png",
+
+priceId: "tron",
+
+chart:
+"https://dexscreener.com/tron/TN3W4H6rK2ce4vX9YnFQHwKENnHjoxb3m9?embed=1"
+
+},
+
+/* USDT TRC20 */
+{
+name: "Tether USD",
+symbol: "USDT",
+
+address:
+"TXLAQ63Xg1NAzckPwKHvzw7CSEmLMEqcdj",
+
+logo: "assets/tokens/usdt.png",
+
+priceId: "tether",
+
+chart:
+"https://dexscreener.com/tron/TXLAQ63Xg1NAzckPwKHvzw7CSEmLMEqcdj?embed=1"
+
+},
+
+/* WRAPPED TRON */
+{
+name: "Wrapped TRON",
+symbol: "WTRX",
+
+address:
+"TNUC9Qb1rRpS5CbWLmNMxXBjyFoydXjWFR",
+
+logo: "assets/tokens/wtrx.png",
+
+priceId: "tron",
+
+chart:
+"https://dexscreener.com/tron/TNUC9Qb1rRpS5CbWLmNMxXBjyFoydXjWFR?embed=1"
+
+}
+
+];
